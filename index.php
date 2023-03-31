c275a35cf841d3870f44db5d6f5a1a4d6bb19e68
yngferro2 <129523871+yngferro2@users.noreply.github.com>
2023-03-31 06:58:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>ONLINE VOTING</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body background="background.jpg">
+    <header>
+      <h1 class="logo"><center><b><left>ONLINE VOTING</left></b></h1 </center>
+      <nav class="navigation">
+        <nav class="navigation">
+
+
+          <a href="#">About</a>
+          <a href="#">Contacts</a>   
+        
+        </nav>
+       
+        <p><b><center><br><br><br><br><br><br><br><br><br><h1>LOGIN AS:</h1></br></br></br></br></br></br></center></b></p>
+        <a href="admin/index.php"</a>
+        <center>    <button class="btnLogin-pop" style="height: 100px ; width: 200px;"><b> ADMIN LOGIN</b></button></center>
+        <a href="index2.php"</a>
+    <center>   <button class="btnLogin-pop" style="height: 100px ; width: 200px;"> <b>VOTER LOGIN</b> </button></center>
+   
+  </nav>
+</header>
